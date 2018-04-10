f0db6beaafef518f011bad09d0dcf4c4f86e6594
Bicen Ye <bicenye@Bicens-MacBook-Pro.local>
2018-04-09 22:48:18 -0700

    for submit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyB-imEw7CPyny_ZBxG192gec6DL7vcKXdo",
@@ -22,16 +23,19 @@ console.log("current time HH:mm  " + currenttime);
 console.log("current time convert to minutes =  " + currenttimeConverted);
 
 
+
 var firsttraintime = ""; //in HH:mm
 var firsttraintimeConverted = moment.duration(firsttraintime).asMinutes() //first train time in minutes
 
 var difftime = currenttimeConverted - firsttraintimeConverted // in minuttes, difference between current time and first train time
 var tRemainder = "" //// in minutes, use Modulus (emainder) to calculate
 var minutestonexttrain = ''; // in minutes
-var keyHolder = '';
-var getKey = '';
 var nextarrival = ""; //in minutes
 var nextarrivalFormatted = ""; //in HH:mm
+var Key = "";
+var intervalId;
+var postData;
+
 
 //time convert function to convert 24 hrs time format to 12 hrs AM/PM hour
 function tConvert (time) {
@@ -46,6 +50,26 @@ return time.join (''); // return adjusted time or original string
 //end of time convert function
 
 $(document).ready(function() {
+// runtimer();
+
+//function to happen every minute
+// function runtimer() {
+//   clearInterval(intervalId);
+//   intervalId = setInterval(updateminute, 20000);
+// }
+
+// //  The update minute function.
+// function updateminute() {
+// console.log("current time HH:mm  " + currenttime);
+// console.log("current time convert to minutes =  " + currenttimeConverted);
+
+//   dataRef.ref().push({
+//    currenttime: currenttime,
+//   });
+// }
+
+
+
 // Capture Button Click
 $("#submit").on("click", function(event) {
     event.preventDefault();
@@ -93,6 +117,35 @@ console.log("next arrival is : " + nextarrivalFormatted);
 //end of onclick event
   });
 
+  //function of updates 
+  // function writeNewPost(trainname,destination,currenttime,firsttraintime,frequency,nextarrivalFormatted,minutestonexttrain) {
+  //   // A post entry.
+  //   var postData = {
+  //     trainname: trainname,
+  //     uid:uid,
+  //     destination: destination,
+  //     currenttime:currenttime,
+  //     firsttraintime: firsttraintime, // HH:mm
+  //     frequency: frequency, // in minutes
+  //     nextarrivalFormatted:nextarrivalFormatted,
+  //     minutestonexttrain:minutestonexttrain,
+  //   };
+
+  //   console.log("new" + postData);
+  
+  //   // Get a key for a new Post.
+  //   var newPostKey = firebase.database().ref().child().push().key;
+  
+  //   // Write the new post's data simultaneously in the posts list and the user's post list.
+  //   var updates = {};
+  //   updates[newPostKey] = postData;
+  
+  //   return firebase.database().ref().update(updates);
+  // }
+//end of update function
+
+
+
 // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
 dataRef.ref().on("child_added", function(childSnapshot) {
 
@@ -100,31 +153,31 @@ dataRef.ref().on("child_added", function(childSnapshot) {
 console.log(childSnapshot.val());
 
 
-$('#train-table').append("<tr class='table-row' >" +
-            "<td>"+ childSnapshot.val().trainname +
-             "</td>" +
-             "<td>" + childSnapshot.val().destination +
-             "</td>" +
-             "<td>" + childSnapshot.val().frequency +
-             "</td>" +
-             "<td>" + childSnapshot.val().nextarrivalFormatted + 
-             "</td>" +
-             "<td>" + childSnapshot.val().minutestonexttrain + "mins" +
-            "<td>" + "<button type='submit' value='Remove' class='remove-train btn btn-primary'>" + "<i class='fa fa-trash'>" + "Remove" + "</td>" + 
+$('#train-table').append("<tr class='table-row' id=" + "'" + childSnapshot.key + "'" + ">" +
+            "<td>"+ childSnapshot.val().trainname +"</td>" +
+             "<td>" + childSnapshot.val().destination +"</td>" +
+             "<td>" + childSnapshot.val().frequency + "mins" +"</td>" +
+             "<td>" + childSnapshot.val().nextarrivalFormatted + "</td>" +
+             "<td>" + childSnapshot.val().minutestonexttrain + "mins" +"<td>" + 
+             "<button type='submit' id = 'remove' class='remove-train btn btn-primary'>" + "<i class='fa fa-trash'>" + "Remove" + "</td>" + 
              "</td>" +
              // lastly add a remove button
         "</tr>"); 
-
+        $("body").on("click", "#remove", function(){
+          dataRef = firebase.database();
+          console.log(this) 
+          $(this).closest ('tr').remove();
+           thiskey = $(this).parent().parent().attr('id');
+           console.log(thiskey);
+           console.log(dataRef.ref().child(thiskey));
+          //  dataRef.child(key).remove();
+        });
 // Handle the errors
 }, function(errorObject){
   //console.log("Errors handled: " + errorObject.code)
+  
 });
 
-$("body").on("click", ".remove-train", function(){
-   $(this).closest ('tr').remove();
-  //  getKey = $(this).parent().parent().attr('id');
-  //  dataRef.child(getKey).remove();
-});
 
 
 //end of document ready
